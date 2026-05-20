963dc2e60d22a330c4f703a929b613628543bd9b
NikitaN04 <nikitanilsson04@hotmail.com>
2026-05-20 19:31:32 +0200

    lagt till i sortbuttons och movieforms

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -8,49 +8,15 @@ function App() {
 
   return (
     <div className="container mt-5">
-
-      <h1 className="mb-4">
-        Min filmlista
-      </h1>
+      <h1 className="mb-4">Min filmlista</h1>
 
       <AddMovieForm />
 
-        <h1>Min filmlista</h1>
-        <form id="add-movie-form">
-            <fieldset>
-                <legend>Lägg till en film</legend>
-
-                <label for="title-field">Titel:</label>
-                <input type="text" id="title-field" class="form-control">
-
-                <label for="rating-field">Betyg:</label>
-
-                <select type="text" id="rating-field" class="form-control">
-                    <option value="0">Välj betyg här...</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>
-
-                <input type="submit" class="btn btn-success mt-3" value="Spara film">
-
-            </fieldset>
-        </form>
-
-        <hr>
-
-        <h2>Filmer</h2>
-
-        <ul id="movies">
-
-        </ul>
-
       <MovieList />
 
-      <SortButtons />
-
+      <SortButtons 
+      onSortByTitle={sortByTitle}
+        onSortByRating={sortByRating}/>
     </div>
   );
 }
