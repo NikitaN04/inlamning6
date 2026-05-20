4ff94b639fdcec1a72a43d4770c082c7a3596881
louiseaholter <holterlouise@gmail.com>
2026-05-20 19:08:55 +0200

    skelett i app.jxs att skriva utifrån

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,3 +3,24 @@ import AddMovieForm from "./components/AddMovieForm";
 import MovieList from "./components/MovieList";
 import SortButtons from "./components/SortButtons";
 
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  return (
+    <div className="container mt-5">
+
+      <h1 className="mb-4">
+        Min filmlista
+      </h1>
+
+      <AddMovieForm />
+
+      <MovieList />
+
+      <SortButtons />
+
+    </div>
+  );
+}
+
+export default App;
