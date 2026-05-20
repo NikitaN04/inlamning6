e97ea235404d25f07f687d32f6a0acf043a956bf
NikitaN04 <nikitanilsson04@hotmail.com>
2026-05-20 20:09:29 +0200

    lagt till med sort knapparna titel och rating

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import AddMovieForm from "./components/AddMovieForm";
 //import MovieList from "./components/MovieList";
-//import SortButtons from "./components/SortButtons";
+import SortButtons from "./components/SortButtons";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -11,11 +11,40 @@ function App() {
     console.log(movie);
   }
 
+  function sortByTitle() {
+    const sorted = [...movies];
+
+    sorted.sort((a, b) => {
+      if (a.title > b.title) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+
+    setMovies(sorted);
+  }
+
+  function sortByRating() {
+    const sorted = [...movies];
+
+    sorted.sort((a, b) => {
+      return b.rating - a.rating;
+    });
+
+    setMovies(sorted);
+  }
+  
   return (
     <div className="container mt-5">
       <h1 className="mb-4">Min filmlista</h1>
 
       <AddMovieForm onAddMovie={addMovie} />
+
+      <SortButtons
+        onSortByTitle={sortByTitle}
+        onSortByRating={sortByRating}
+      />
 
 
     </div>
